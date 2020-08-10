c982a3fcafafe9368bd40e62f4fb0ee33e74745d
Siful Islam <sifulsmm@gmail.com>
2020-08-10 14:45:08 +0600

    first react commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Note() {
+    return <div className="note">
+        <h1>This is for Title</h1>
+        <p> This one is for contents</p>
+    </div>
+}
+
+export default Note;
