1649251c8d670bf41ce30eab88022c821eaef73b
Siful Islam <sifulsmm@gmail.com>
2020-08-15 19:25:01 +0600

    calculator and pin generator

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App"; 
 
-ReactDOM.render(
+ReactDOM.render( 
     <App />,
     document.getElementById("root")
 )
